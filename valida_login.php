8fca0311869a875ebf1eee6eb50f6433c587dbe6
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-29 18:18:37 -0300

    Navegação entre páginas

--- valida_login.php
+++ valida_login.php
@@ -27,6 +27,7 @@
     if($usuario_autenticado) {
         echo 'Usuário autenticado';
         $_SESSION['autenticado'] = 'SIM';
+        header('Location: home.php');
     } else {
         $_SESSION['autenticado'] = 'NAO';
         header('Location: index.php?login=erro');
