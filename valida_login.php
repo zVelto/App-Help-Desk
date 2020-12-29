4bd2307d4ff12df79ef0d479fe6605a00ada04a7
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-28 22:01:34 -0300

    Autenticando usuário

--- valida_login.php
+++ valida_login.php
@@ -1,5 +1,33 @@
 <?php
 
+//variável que verifica se a autentificação foi realizada
+$usuario_autenticado = false;
+
+    //usuarios do sistema
+    $usuarios_app = array(
+        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+        array('email' => 'user@teste.com.br', 'senha' => 'abcd')
+    );
+
+    /*
+    echo '<pre>';
+    print_r($usuarios_app);
+    echo '</pre>';
+    */
+
+    foreach($usuarios_app as $user) {
+
+        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+            $usuario_autenticado = true;
+        }
+    }
+
+    if($usuario_autenticado) {
+        echo 'Usuário autenticado';
+    } else {
+        header('Location: index.php?login=erro');
+    }
+
     /*
     print_r($_GET);
 
@@ -10,6 +38,7 @@
     echo $_GET['senha'];
     */
 
+    /*
     print_r($_POST);
 
     echo '<br>';
@@ -17,4 +46,5 @@
     echo $_POST['email'];
     echo '<br>';
     echo $_POST['senha'];
+    */
 ?>
